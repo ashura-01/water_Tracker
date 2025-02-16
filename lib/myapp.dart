d268bb4816d8aff317c73a5b3ccc3e2e2937250d
MD Fahim <fmn.flame@gmail.com>
2025-02-16 22:26:34 +0600

    changes

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'package:sizer/sizer.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return Sizer(builder: (context, orientation, screenType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      );
+    });
   }
-
 }
